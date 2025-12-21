e519ee60f1006906a45251d39e6b37427c0ce542
alexanderdeloecker <Alexander.de.loecker@student.ehb.be>
2025-12-21 15:58:50 +0100

    JWT aangepast

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,5 +1,9 @@
 import jwt from "jsonwebtoken";
 
+if (!process.env.JWT_SECRET) {
+	throw new Error("JWT_SECRET is not set");
+}
+
 export function signToken(payload) {
 	return jwt.sign(payload, process.env.JWT_SECRET, {
 		expiresIn: "7d",
